797d2086f849f80c62f36594d978d2c446fb2df0
Koshelev Mikhail <mihakosh@gmail.com>
2023-03-04 01:52:20 +0300

    Adding pizeo action to photoresitor

--- main.uno/main.uno.ino
+++ main.uno/main.uno.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#define PIN_PHOTO_SENSOR A0
+#define PIN_PIEZO 12
+#define PIN_DIOD 8
+#define HIGH_TONE 1000
+#define LOW_TONE 500
+#define TACT_COUNT 5
+#define DELAY_PIEZO 500
+bool light = 1;
 
+void setup() {
+  pinMode(PIN_DIOD, 1);
+  Serial.begin(9600);
+  Serial.print("Hello\n");
+}
+
+void light_low(){
+  for (int i = 0; i < TACT_COUNT; ++i) {
+    tone(PIN_PIEZO, HIGH_TONE, DELAY_PIEZO);
+    delay(DELAY_PIEZO);
+    tone(PIN_PIEZO, LOW_TONE, DELAY_PIEZO);
+    delay(DELAY_PIEZO);
+  }
+  light = 1;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int photo_sensor_value = analogRead(PIN_PHOTO_SENSOR);
+  if (photo_sensor_value >= 1000) {
+    digitalWrite(PIN_DIOD, 1);
+    if (!light) {
+      light_low();
+    }
+  } else {
+    digitalWrite(PIN_DIOD, 0);
+    noTone(PIN_PIEZO);
+    light = 0;
+  }
 }
