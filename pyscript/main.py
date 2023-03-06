b72f804d8d08d8ab1a1260a57db76966bc2b543a
Koshelev Mikhail <mihakosh@gmail.com>
2023-03-06 21:25:48 +0300

    ~pyscript v1

--- pyscript/main.py
+++ pyscript/main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def update_canvas(x_axis_data, y_axis_data, fig, ax, x_lim, y_lim):
+    ax.clear()
+    ax.plot(x_axis_data, y_axis_data)
+    plt.xlabel(x_label, fontsize=14)
+    plt.xlabel(y_label, fontsize=14)
+    ax.set(xlim=(0, x_lim), ylim=(0, y_lim))
+    plt.draw()
+    plt.pause(0.01)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        exit(1)
+    x_label = sys.argv[1]
+    y_label = sys.argv[2]
+    data_file = sys.argv[3]
+    lock_file = sys.argv[4]
+
+    x_axis_data = [0]
+    y_axis_data = [0]
+
+    fig, ax = plt.subplots(1, 1)
+    update_canvas(x_axis_data, y_axis_data, fig, ax, 1, 1)
+    max_data = 0.0
+
+    while True:
+        lock_f = open(lock_file, 'r')
+        if lock_f.read() == '\0':
+            data_f = open(data_file, 'r')
+            x_axis_data.append(len(x_axis_data))
+            value_data = float(data_f.read())
+            max_data = max(max_data, value_data)
+            y_axis_data.append(value_data)
+            update_canvas(x_axis_data, y_axis_data, fig, ax, len(x_axis_data), max_data + 1)
+            data_f.close()
+
+            lock_fw = open(lock_file, 'w')
+            lock_fw.write('\n')
+            lock_fw.close()
+        elif lock_f.read() == '\t':
+            break
+        lock_f.close()
