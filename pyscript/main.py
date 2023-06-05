a389c38d83b00d0e71f3f3362cfa82e7b5032179
Koshelev Mikhail <mihakosh@gmail.com>
2023-06-05 03:49:55 +0300

    demo v0.1.0

--- pyscript/main.py
+++ pyscript/main.py
@@ -5,7 +5,22 @@ import os
 import struct
 
 
-def update_canvas(x_axis_data, y_axis_data, fig, ax, y_lim, y_low_lim):
+def set_labels(ax):
+    ax[0, 0].set_title('Preasure')
+    ax[0, 1].set_title('Temp')
+    ax[1, 0].set_title('Sound')
+    ax[1, 1].set_title('Light')
+    ax[0, 0].set_xlabel('time')
+    ax[0, 0].set_ylabel('kPa')
+    ax[0, 1].set_xlabel('time')
+    ax[0, 1].set_ylabel('C°')
+    ax[1, 0].set_xlabel('time')
+    ax[1, 0].set_ylabel('Db')
+    ax[1, 1].set_xlabel('time')
+    ax[1, 1].set_ylabel('L')
+
+
+def update_canvas(x_axis_data, y_axis_data, fig, ax):
     for i in range(2):
         for j in range(2):
             ax[i, j].clear()
@@ -13,10 +28,7 @@ def update_canvas(x_axis_data, y_axis_data, fig, ax, y_lim, y_low_lim):
             u_lim = max(y_axis_data[i * 2 + j]) + 1
             d_lim = min(y_axis_data[i * 2 + j]) - 1
             ax[i, j].set(xlim=(x_axis_data[i * 2 + j][0], x_axis_data[i * 2 + j][-1]), ylim=(d_lim, u_lim))
-    ax[0, 0].set_title('Preasure')
-    ax[0, 1].set_title('Temp')
-    ax[1, 0].set_title('Sound')
-    ax[1, 1].set_title('Light')
+    set_labels(ax)
     plt.draw()
     plt.pause(0.01)
 
@@ -24,12 +36,7 @@ def update_canvas(x_axis_data, y_axis_data, fig, ax, y_lim, y_low_lim):
 if __name__ == "__main__":
     x_axis_data = [[], [], [], []]
     y_axis_data = [[], [], [], []]
-
     fig, ax = plt.subplots(2, 2)
-#    update_canvas(x_axis_data, y_axis_data, fig, ax, [1, 1, 1, 1], [0, 0, 0, 0])
-    max_data = [0.0, 0.0, 0.0, 0.0]
-    min_data = [0.0, 0.0, 0.0, 0.0]
-
     pipe = open('/tmp/my_pipe', 'rb')
     while True:
         for i in range(4):
@@ -38,13 +45,11 @@ if __name__ == "__main__":
                 x_axis_data[i].append(0)
             else:
                 x_axis_data[i].append(x_axis_data[i][-1] + 1)
-            max_data[i] = max(max_data[i], current)
-            min_data[i] = max(min_data[i], current)
             y_axis_data[i].append(current)
             if len(x_axis_data[i]) > 50:
                 x_axis_data[i].pop(0)
                 y_axis_data[i].pop(0)
 
-        update_canvas(x_axis_data, y_axis_data, fig, ax, max_data, min_data)
+        update_canvas(x_axis_data, y_axis_data, fig, ax)
 
     pipe.close()
